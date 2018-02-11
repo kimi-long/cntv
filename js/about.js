16f0b6e53f243a492c8825b82c3323088a9c6c39
kimi-long <745222170@qq.com>
2018-02-11 16:42:28 +0800

    about重构2

--- js/about.js
+++ js/about.js
@@ -1,5 +1,9 @@
 $(function () {
     $(".aboutLab-li").click(function () {
-        
+        $(".aboutLab-li").removeClass("aboutLab-active");
+        $(this).addClass("aboutLab-active")
+        $(".about-main-part").addClass("hide")
+        let showPart=$(this).attr("data-infor");
+        $('.'+showPart).removeClass("hide")
     })
 })
